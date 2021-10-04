a270121ee4be340dd20ba9e0cff2a63f5ea88070
André Biseth <andre@biseth.net>
2021-10-04 08:35:12 +0200

    Working with device

--- drivers/kona/driver.ts
+++ drivers/kona/driver.ts
@@ -1,4 +1,6 @@
 import { Driver } from 'homey';
+import BlueLinky from "bluelinky";
+import PairSession from 'homey/lib/PairSession';
 
 class MyDriver extends Driver {
 
@@ -7,6 +9,78 @@ class MyDriver extends Driver {
    */
   async onInit() {
     this.log('MyDriver has been initialized');
+  }
+
+  async getVehicles() {
+
+    return new Promise((resolve, reject) => {
+      const t = setTimeout(() => {
+        reject('Login timed out.');
+      }, 30000);
+
+      const client = new BlueLinky({
+        username: '',
+        password: '',
+        brand: 'hyundai',
+        region: 'EU',
+        pin: ''
+      });
+
+      client.on('ready', async () => {
+        const vehicles = await client.getVehicles();
+        try {
+          resolve(vehicles);
+        } catch (err) {
+          reject(err);
+        }
+      });
+
+      client.on('error', async (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  async onPair(session: PairSession) {
+    let username = "";
+    let password = "";
+
+    session.setHandler("login", async (data) => {
+      username = data.username;
+      password = data.password;
+
+      const credentialsAreValid = await DeviceAPI.testCredentials({
+        username,
+        password,
+      });
+
+      // return true to continue adding the device if the login succeeded
+      // return false to indicate to the user the login attempt failed
+      // thrown errors will also be shown to the user
+      return credentialsAreValid;
+    });
+
+    session.setHandler("list_devices", async () => {
+      const api = await DeviceAPI.login({ username, password });
+      const myDevices = await api.getDevices();
+
+      const devices = myDevices.map((myDevice) => {
+        return {
+          name: myDevice.name,
+          data: {
+            id: myDevice.id,
+          },
+          settings: {
+            // Store username & password in settings
+            // so the user can change them later
+            username,
+            password,
+          },
+        };
+      });
+
+      return devices;
+    });
   }
 
   /**
